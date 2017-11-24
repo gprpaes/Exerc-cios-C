5c7b92b92190e8d54e5e09d732c702cab3c9e6ea
Guilherme Paes <guilhermep98@gmail.com>
2017-11-24 10:36:36 -0200

    Avanços no exercicio do maior fator primo

--- maior_fator_primo.c
+++ maior_fator_primo.c
@@ -1,26 +1,33 @@
-// Programa que calcula o maior fator primo de 600851475143
+// Programa que calcula o maior fator primo de 600851475143 usando o Crivo de Erastostenes
 
 #include <stdio.h>
+#include <math.h>
 
+
+unsigned long int crivo_erastostenes(long int valor);
 
 int main(void)
 {
-	long int n = 600851475143;
-	long int divisor;
-	
-	for(long int i = 2; i<=n; i++)
-	{
-		if(n % i == 0)
-		{
-			
-			
-			
-		}
-		
-	}
+	long int valor, limite;
+	printf("Digite o valor que deseja decompor: ");
+	scanf("%ld", &valor);
+	limite = crivo_erastostenes(valor);
+	printf("O limite do crivo é: \n%ld", limite);
 	
 	
 	
 	return 0;
 }
 
+
+unsigned long int crivo_erastostenes(long int valor)
+{
+	
+	long limite = (long) sqrt(valor);
+	return limite;
+	
+	
+	
+	
+	
+}
