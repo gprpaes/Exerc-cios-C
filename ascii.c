090e9c200f2cf14778bcbf375622638c4458eb69
Guilherme Paes <guilhermep98@gmail.com>
2017-12-24 04:05:57 -0200

    ascii.c

---
+++ ascii.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+int main (void){
+	
+	char ascii;
+	int aux;
+	
+	printf("Digite um número: ");
+	scanf("%d", &aux);
+	
+	ascii = aux;
+	
+	printf("%c", ascii); 
+	
+	
+	
+	
+	return 0;
+}
